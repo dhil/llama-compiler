f14b7c048a6d78814a4c93d5421fe0e4276e0e1a
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2018-06-20 16:42:12 +0100

    Adds Llama built-ins

---
+++ llama_Builtins.ml
@@ -0,0 +1,3 @@
+(** Definition of Llama built-ins **)
+let print_int_nl : int -> unit
+  = fun n -> Printf.printf "%d\n%!" n
---
+++ llama_Builtins.mli
@@ -0,0 +1,2 @@
+(** Signatures for Llama built-ins **)
+val print_int_nl : int -> unit
